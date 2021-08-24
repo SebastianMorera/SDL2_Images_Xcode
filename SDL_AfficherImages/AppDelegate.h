b63c42b98f97fec19c4fa4bbf82970b42f40b5d8
Sebastian Morera <sebas.morera999@hotmail.com>
2021-08-23 20:28:30 -0400

    Initial Commit

---
+++ SDL_AfficherImages/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SDL_AfficherImages
+//
+//  Created by Sebastian Morera on 2021-08-23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
